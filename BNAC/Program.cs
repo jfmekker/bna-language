cd77f6f13a9c5283029d2d40374a3ab84fa92513
jfmekker <jfm8746@rit.edu>
2020-04-15 00:19:07 -0700

    Working example tokenizer.

--- BNAC/Program.cs
+++ BNAC/Program.cs
@@ -10,6 +10,30 @@ namespace BNAC
 	{
 		static void Main( string[] args )
 		{
+			Console.WriteLine( "================================================================================" );
+			Console.WriteLine( "Welcome to the BNA's Not Assembly Compiler!" );
+			Console.WriteLine( "================================================================================" );
+
+			while ( true ) {
+				Console.WriteLine( "\nWrite a line to be tokenized:" );
+
+				// get a line
+				var input = Console.ReadLine( );
+
+				// end for empty lines (for now)
+				if ( input.Equals( "" ) )
+					break;
+
+				try {
+					var queue = Token.TokenizeLine( input );
+					foreach ( Token t in queue )
+						Console.WriteLine( t.ToString( ) );
+				}
+				catch (Exception e) {
+					Console.Error.WriteLine( e.Message );
+				}
+			}
+
 		}
 	}
 }
---
+++ BNAC/Token.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BNAC
+{
+	
+	class Token
+	{
+		const char LABEL_END = ':';
+
+		public enum TokenType
+		{
+			UNKNOWN,
+
+			KEYWORD,
+			LITERAL,
+			VARIABLE,
+
+			LABEL_END,
+
+			// Operation start keywords
+			SET,
+			ADD,
+
+			// Operation mid keywords
+			TO,
+
+			PRINT,
+		}
+
+		public TokenType Type
+		{
+			get; set;
+		}
+
+		public string Value
+		{
+			get; set;
+		}
+
+		private Token( string value, TokenType type = TokenType.UNKNOWN )
+		{
+			Value = value;
+			Type = type;
+
+			// Try to identify type
+			if ( Type == TokenType.UNKNOWN ) {
+				// Number Literal
+				if ( char.IsDigit( Value[0] ) ) {
+					if ( int.TryParse( Value , out int val ) ) {
+						Type = TokenType.LITERAL;
+					}
+					else {
+						throw new System.Exception( "Failed to parse literal value: '" + Value + "'" );
+					}
+				}
+
+				// Keywords
+				else if ( Value.ToUpper( ).Equals( "SET" ) ) {
+					Type = TokenType.SET;
+				}
+				else if ( Value.ToUpper( ).Equals( "ADD" ) ) {
+					Type = TokenType.ADD;
+				}
+				else if ( Value.ToUpper( ).Equals( "TO" ) ) {
+					Type = TokenType.TO;
+				}
+
+				// Variable
+				else {
+					Type = TokenType.VARIABLE;
+				}
+			}
+		}
+
+		public static Queue<Token> TokenizeLine( string line )
+		{
+			var tokens = new Queue<Token>( );
+
+			// First, check if this is a comment
+			if ( line[0] == '#' )
+				return tokens;
+
+			// Parse character by character
+			string candidate = "";
+			foreach ( char c in line ) {
+				switch ( c ) {
+					case LABEL_END:
+						tokens.Enqueue( new Token( candidate ) );
+						candidate = "";
+						tokens.Enqueue( new Token( c.ToString(), TokenType.LABEL_END ) );
+						break;
+
+					case ' ':
+					case '\t':
+						if ( candidate.Length > 0 ) 
+							tokens.Enqueue( new Token( candidate ) );
+						candidate = "";
+						break;
+
+					default:
+						if ( char.IsLetterOrDigit( c ) ) {
+							candidate += c;
+							break;
+						}
+						throw new System.Exception( "Illegal symbol: '" + c + "' (" + ( (uint)c ).ToString( ) + ")");
+				}
+			}
+
+			// Add last candidate
+			if ( candidate.Length > 0 )
+				tokens.Enqueue( new Token( candidate ) );
+
+			return tokens;
+		}
+
+
+		public static Queue<Token> TokenizeProgram( Queue<string> lines )
+		{
+			var tokens = new Queue<Token>( );
+
+			while ( lines.Count > 0 ) {
+				var line_tokens = TokenizeLine( lines.Dequeue() );
+				while ( line_tokens.Count > 0 ) {
+					tokens.Enqueue( line_tokens.Dequeue( ) );
+				}
+			}
+
+			return tokens;
+		}
+
+		public override string ToString( )
+		{
+			return "'" + Value + "' (" + Type + ")";
+		}
+	}
+}
