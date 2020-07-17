b820878133abe3ee7c97a2e1addacfddd3a4834a
jfmekker <main@jacobmekker.com>
2020-07-17 14:19:35 -0700

    Started BNAVM project. Started common BNAB project. Created binary specification.

---
+++ BNAVM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNAVM
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "================================================================================" );
+			Console.WriteLine( "Welcome to the BNA's Not Assembly Virtual Machine!" );
+			Console.WriteLine( "================================================================================" );
+
+
+		}
+	}
+}
