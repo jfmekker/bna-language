e43b08396d8492f55de2545be66853e7e724edff
jfmekker <main@jacobmekker.com>
2021-04-12 18:56:24 -0700

    Changed handling of command line arguments.

--- BNA/BNA.cs
+++ BNA/BNA.cs
@@ -98,43 +98,54 @@ namespace BNA
 			// else, take in files
 			else {
 				foreach ( string file in args ) {
+					// Output the BNA filename
+					Console.WriteLine( file + ":" );
+
+					// Check the file extension
+					string[] split_filename = file.Split( new char[] { '.' } );
+					string filename = split_filename[0];
+					string extension = split_filename[1];
+					if ( !extension.Equals( "bna" ) ) {
+						throw new Exception( "Wrong file type: " + file );
+					}
+
+					// Output file contents
+					Console.WriteLine( "Reading file..." );
+					var lines = new Queue<string>( );
+					using ( StreamReader sr = File.OpenText( ".\\" + file ) ) {
+						string line;
+						while ( ( line = sr.ReadLine( ) ) != null ) {
+							Console.WriteLine( line );
+							lines.Enqueue( line );
+						}
+					}
+
+					// Compile to program and run
 					try {
-						// Output the BNA filename
-						Console.WriteLine( file + ":" );
-
-						// Check the file extension
-						string[] split_filename = file.Split( new char[] { '.' } );
-						string filename = split_filename[0];
-						string extension = split_filename[1];
-						if ( !extension.Equals( "bna" ) ) {
-							throw new Exception( "Wrong file type: " + file );
-						}
-
-						// Output file contents
-						Console.WriteLine( "Reading file..." );
-						var lines = new Queue<string>( );
-						using ( StreamReader sr = File.OpenText( ".\\" + file ) ) {
-							string line;
-							while ( ( line = sr.ReadLine( ) ) != null ) {
-								Console.WriteLine( line );
-								lines.Enqueue( line );
-							}
-						}
-
-						// Compile it
-						Console.WriteLine( "Compiling file..." );
-						Compile( lines );
-
-						Console.WriteLine( "\nDone.\n" );
+						Console.WriteLine( "Compiling Program..." );
+						var prog = new Program( Compile( lines ) );
+						Console.WriteLine( "\nRunning Program...\n" );
+						prog.Run( );
+						Console.WriteLine( );
 					}
-					catch ( Exception e ) {
-						Console.Error.WriteLine( "!!! Caught Exception compiling file !!!:" );
-						Console.Error.WriteLine( e.Message );
+					catch ( CompiletimeException e ) {
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.WriteLine( "Compiletime Exception caught:" );
+						Console.WriteLine( e.Message );
+						Console.ResetColor( );
 					}
+					catch ( RuntimeException e ) {
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.WriteLine( "Runtime Exception caught:" );
+						Console.WriteLine( e.Message );
+						Console.ResetColor( );
+					}
+
+					Console.WriteLine( "\nDone with file.\n" );
 				}
 
 				// Wait to close so user can read output
-				Console.WriteLine( "\nPress enter to exit..." );
+				Console.WriteLine( "\nFinished, press enter to exit..." );
 				Console.ReadLine( );
 			}
 		}
