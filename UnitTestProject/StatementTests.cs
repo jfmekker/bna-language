7040e4cf3919607a5920ec520d659a85f13827b8
jfmekker <main@jacobmekker.com>
2021-04-25 22:21:01 -0700

    Added calls to statement tests for all statements.

--- UnitTestProject/StatementTests.cs
+++ UnitTestProject/StatementTests.cs
@@ -19,12 +19,6 @@ namespace UnitTestProject
 		}
 
 		[TestMethod]
-		public void PrintTest( )
-		{
-			RunStatementTest( "print" );
-		}
-
-		[TestMethod]
 		public void SetTest( )
 		{
 			RunStatementTest( "set" );
@@ -34,6 +28,124 @@ namespace UnitTestProject
 		public void AddTest( )
 		{
 			RunStatementTest( "add" );
+		}
+
+		[TestMethod]
+		public void SubtractTest( )
+		{
+			RunStatementTest( "subtract" );
+		}
+
+		[TestMethod]
+		public void MultiplyTest( )
+		{
+			RunStatementTest( "multiply" );
+		}
+
+		[TestMethod]
+		public void DivideTest( )
+		{
+			RunStatementTest( "divide" );
+		}
+
+		[TestMethod]
+		public void AndTest( )
+		{
+			RunStatementTest( "and" );
+		}
+
+		[TestMethod]
+		public void OrTest( )
+		{
+			RunStatementTest( "or" );
+		}
+
+		[TestMethod]
+		public void XorTest( )
+		{
+			RunStatementTest( "xor" );
+		}
+
+		[TestMethod]
+		public void ModTest( )
+		{
+			RunStatementTest( "mod" );
+		}
+
+		[TestMethod]
+		public void LogTest( )
+		{
+			RunStatementTest( "log" );
+		}
+
+		[TestMethod]
+		public void RaiseTest( )
+		{
+			RunStatementTest( "raise" );
+		}
+
+		[TestMethod]
+		public void NegateTest( )
+		{
+			RunStatementTest( "negate" );
+		}
+
+		[TestMethod]
+		public void RoundTest( )
+		{
+			RunStatementTest( "round" );
+		}
+
+		[TestMethod]
+		public void RandomTest( )
+		{
+			RunStatementTest( "random" );
+		}
+
+		[TestMethod]
+		public void WaitTest( )
+		{
+			RunStatementTest( "wait" );
+		}
+
+		[TestMethod]
+		public void TestTest( )
+		{
+			RunStatementTest( "test" );
+		}
+
+		[TestMethod]
+		public void GotoTest( )
+		{
+			RunStatementTest( "goto" );
+		}
+
+		[TestMethod]
+		public void ListTest( )
+		{
+			RunStatementTest( "list" );
+		}
+
+		[TestMethod]
+		public void AppendTest( )
+		{
+			RunStatementTest( "append" );
+		}
+
+		[TestMethod]
+		public void InputTest( )
+		{
+			// Inconclusive instead of fail until we figure out
+			//  how to mock user input in an automated test
+			Assert.Inconclusive( );
+
+			//RunStatementTest( "input" );
+		}
+
+		[TestMethod]
+		public void PrintTest( )
+		{
+			RunStatementTest( "print" );
 		}
 	}
 }
