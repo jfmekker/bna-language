7973a4a4ad78fb7cd37100514d514a8e8826293b
jfmekker <main@jacobmekker.com>
2021-04-28 14:25:28 -0700

    Refactored unit test project.

---
+++ UnitTestProject/TestRunner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+
+	public static class TestRunner
+	{
+		public static void RunTestFile( string filename , bool fails = false )
+		{
+			BNA.ReturnCode r = BNA.ReturnCode.UNEXPECTED_ERROR;
+
+			try {
+				r = BNA.BNA.RunFromFiles( new string[] { "../../../../Tests/" + filename + ".bna" } );
+			}
+			catch ( Exception e ) {
+				Assert.Inconclusive( );
+			}
+
+			if ( r == BNA.ReturnCode.FILE_ERROR ) {
+				Assert.Inconclusive( );
+			}
+
+			Assert.AreEqual( !fails ? BNA.ReturnCode.SUCCESS : BNA.ReturnCode.BNA_ERROR , r );
+		}
+	}
+}
